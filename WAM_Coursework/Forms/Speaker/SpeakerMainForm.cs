13bfcee82c01aba13406019be48dc3c83812b583
Woody Taylor <woodyktaylor@icloud.com>
2026-04-24 18:33:20 +0100

    showing all submitted talks

--- WAM_Coursework/Forms/Speaker/SpeakerMainForm.cs
+++ WAM_Coursework/Forms/Speaker/SpeakerMainForm.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Forms;
+﻿using CsvHelper.Configuration.Attributes;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using WAM_Coursework.Conferences;
+using WAM_Coursework.FileHandlers;
+using WAM_Coursework.Users;
 
 namespace WAM_Coursework.Forms
 {
@@ -7,12 +12,32 @@ namespace WAM_Coursework.Forms
         public SpeakerMainForm()
         {
             InitializeComponent();
+            UpdateTalkList();
         }
 
         private void NewTalkButton_Click(object sender, System.EventArgs e)
         {
             SubmitTalkForm submitForm = new SubmitTalkForm();
             submitForm.ShowDialog();
+            UpdateTalkList();
+        }
+
+        private void UpdateTalkList()
+        {
+            ApplicationsFlowPanel.Controls.Clear();
+            string userEmail = CurrentUser.Instance.User.record.Email;
+            List<TalkRecord> talks = FileManager.ReadRecords<TalkRecord>(FileManager.StorageFile.talks);
+            List<TalkRecord> speakerTalks = talks.FindAll(t => t.SpeakerEmail ==userEmail);
+
+            foreach (var talk in speakerTalks)
+            {
+                Label talkLabel = new Label() { Text = talk.Title + " - " + talk.Description };
+                talkLabel.Font = new System.Drawing.Font(talkLabel.Font.FontFamily, 20);
+                talkLabel.ForeColor = System.Drawing.Color.White;
+                talkLabel.AutoSize = true;
+                talkLabel.BorderStyle = BorderStyle.FixedSingle;
+                ApplicationsFlowPanel.Controls.Add(talkLabel);
+            }
         }
     }
 }
--- WAM_Coursework/Forms/Start/LoginForm.cs
+++ WAM_Coursework/Forms/Start/LoginForm.cs
@@ -14,8 +14,31 @@ namespace WAM_Coursework.Forms
         private void LogInButton_Click(object sender, System.EventArgs e)
         {
             User.AttemptLogin(EmailTextBox.Text, PasswordTextBox.Text);
-            SubmitTalkForm submitForm = new SubmitTalkForm();
-            submitForm.Show();
+            if (CurrentUser.Instance.User == null)
+            {
+                MessageBox.Show("Invalid email or password. Please try again.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            switch (CurrentUser.Instance.User.record.Role)
+            {
+                case UserConstants.SpeakerRole:
+                    SpeakerMainForm SpeakerForm = new SpeakerMainForm();
+                    SpeakerForm.Show();
+                    Close();
+                    break;
+                case UserConstants.ReviewerRole:
+                    ReviewerMainForm ReviewerForm = new ReviewerMainForm();
+                    ReviewerForm.Show();
+                    Close();
+                    break;
+                case UserConstants.ManagerRole:
+                    ManagerMainForm ManagerForm = new ManagerMainForm();
+                    ManagerForm.Show();
+                    Close();
+                    break;
+                default:
+                    break;
+            }        
         }
     }
 }
