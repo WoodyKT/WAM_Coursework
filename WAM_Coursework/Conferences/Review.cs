9cb892f910868537a228b065aca4afc3388dca7a
Woody Taylor <woodyktaylor@icloud.com>
2026-04-25 18:08:50 +0100

    Added showing reviews, clicking them shows details

--- WAM_Coursework/Conferences/Review.cs
+++ WAM_Coursework/Conferences/Review.cs
@@ -1,7 +1,15 @@
 ﻿namespace WAM_Coursework.Conferences
 {
-    internal class Review
+    public class Review
     {
+       ReviewRecord record = new ReviewRecord();
+
+        public Review(int score, string comment, int reviewId)
+        {
+            record.Score = score;
+            record.Comment = comment;
+            record.ReviewId = reviewId;
+        }
 
     }
 }
--- WAM_Coursework/Forms/Reviewer/ReviewerMainForm.cs
+++ WAM_Coursework/Forms/Reviewer/ReviewerMainForm.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using WAM_Coursework.Conferences;
+using WAM_Coursework.FileHandlers;
+using WAM_Coursework.Users;
+using System;
 
 namespace WAM_Coursework.Forms
 {
@@ -7,7 +12,35 @@ namespace WAM_Coursework.Forms
         public ReviewerMainForm()
         {
             InitializeComponent();
+            UpdateReadyToReviewList();
 
         }
+
+        private void UpdateReadyToReviewList()
+        {
+            ReviewsFlowPanel.Controls.Clear();
+            string userEmail = CurrentUser.Instance.User.record.Email;
+            List<TalkRecord> Talks = FileManager.ReadRecords<TalkRecord>(FileManager.StorageFile.talks);
+            List<TalkRecord> Unreviewed = Talks.FindAll(t => t.ReviewPassed == false);
+
+            foreach (var talk in Unreviewed)
+            {
+                Button talkbutton = new Button() { Text = talk.Title + "\t - " + talk.Description + "\t- " + (talk.ReviewPassed ? "Approved" : "Pending") };
+                talkbutton.Font = new System.Drawing.Font(talkbutton.Font.FontFamily, 20);
+                talkbutton.BackColor = System.Drawing.Color.FromArgb(54, 54, 54);
+                talkbutton.ForeColor = System.Drawing.Color.White;
+                talkbutton.AutoSize = true;
+                talkbutton.Click += (sender, e) => OpenReview(sender,e,talk.Id);
+                ReviewsFlowPanel.Controls.Add(talkbutton);
+            }
+            CountBadge.Text = Unreviewed.Count.ToString() + " remaining";
+        }
+
+        private void OpenReview(object sender, EventArgs e, int talkId) 
+        {
+            SubmitReviewForm submitForm = new SubmitReviewForm(talkId.ToString());
+            submitForm.ShowDialog();
+        }
+
     }
 }
