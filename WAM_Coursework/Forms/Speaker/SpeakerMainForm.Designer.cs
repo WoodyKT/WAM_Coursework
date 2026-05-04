c252addf1f6228eea278164005be16abf20335ee
Alessandro Ciraolo <alex.ciraolo04@gmail.com>
2026-05-04 19:52:41 +0100

    Proper removal of the empty load method
    Thanks for the explanation Woody, I've removed it properly now.

--- WAM_Coursework/Forms/Speaker/SpeakerMainForm.Designer.cs
+++ WAM_Coursework/Forms/Speaker/SpeakerMainForm.Designer.cs
@@ -47,10 +47,10 @@
             this.TitleLabel.AutoSize = true;
             this.TitleLabel.Font = new System.Drawing.Font("Segoe UI", 28F, System.Drawing.FontStyle.Bold);
             this.TitleLabel.ForeColor = System.Drawing.Color.White;
-            this.TitleLabel.Location = new System.Drawing.Point(43, 30);
-            this.TitleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.TitleLabel.Location = new System.Drawing.Point(59, 45);
+            this.TitleLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.TitleLabel.Name = "TitleLabel";
-            this.TitleLabel.Size = new System.Drawing.Size(234, 62);
+            this.TitleLabel.Size = new System.Drawing.Size(331, 88);
             this.TitleLabel.TabIndex = 0;
             this.TitleLabel.Text = "F@irView";
             // 
@@ -59,10 +59,10 @@
             this.SubtitleLabel.AutoSize = true;
             this.SubtitleLabel.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.SubtitleLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
-            this.SubtitleLabel.Location = new System.Drawing.Point(45, 103);
-            this.SubtitleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SubtitleLabel.Location = new System.Drawing.Point(62, 154);
+            this.SubtitleLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.SubtitleLabel.Name = "SubtitleLabel";
-            this.SubtitleLabel.Size = new System.Drawing.Size(129, 23);
+            this.SubtitleLabel.Size = new System.Drawing.Size(182, 32);
             this.SubtitleLabel.TabIndex = 1;
             this.SubtitleLabel.Text = "My applications";
             // 
@@ -75,10 +75,10 @@
             this.NewTalkButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.NewTalkButton.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.NewTalkButton.ForeColor = System.Drawing.Color.White;
-            this.NewTalkButton.Location = new System.Drawing.Point(880, 49);
-            this.NewTalkButton.Margin = new System.Windows.Forms.Padding(4);
+            this.NewTalkButton.Location = new System.Drawing.Point(1210, 74);
+            this.NewTalkButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.NewTalkButton.Name = "NewTalkButton";
-            this.NewTalkButton.Size = new System.Drawing.Size(267, 44);
+            this.NewTalkButton.Size = new System.Drawing.Size(367, 66);
             this.NewTalkButton.TabIndex = 2;
             this.NewTalkButton.Text = "+ New Talk";
             this.NewTalkButton.UseVisualStyleBackColor = false;
@@ -88,20 +88,20 @@
             // 
             this.CardContainerPanel.BackColor = System.Drawing.Color.Transparent;
             this.CardContainerPanel.Controls.Add(this.ApplicationsFlowPanel);
-            this.CardContainerPanel.Location = new System.Drawing.Point(43, 148);
-            this.CardContainerPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.CardContainerPanel.Location = new System.Drawing.Point(59, 222);
+            this.CardContainerPanel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.CardContainerPanel.Name = "CardContainerPanel";
-            this.CardContainerPanel.Size = new System.Drawing.Size(1104, 665);
+            this.CardContainerPanel.Size = new System.Drawing.Size(1518, 998);
             this.CardContainerPanel.TabIndex = 3;
             // 
             // ApplicationsFlowPanel
             // 
             this.ApplicationsFlowPanel.AutoScroll = true;
             this.ApplicationsFlowPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.ApplicationsFlowPanel.Location = new System.Drawing.Point(11, 10);
-            this.ApplicationsFlowPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.ApplicationsFlowPanel.Location = new System.Drawing.Point(15, 15);
+            this.ApplicationsFlowPanel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.ApplicationsFlowPanel.Name = "ApplicationsFlowPanel";
-            this.ApplicationsFlowPanel.Size = new System.Drawing.Size(1083, 645);
+            this.ApplicationsFlowPanel.Size = new System.Drawing.Size(1489, 968);
             this.ApplicationsFlowPanel.TabIndex = 0;
             this.ApplicationsFlowPanel.WrapContents = false;
             // 
@@ -110,9 +110,10 @@
             this.LogoutLabel.AutoSize = true;
             this.LogoutLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LogoutLabel.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
-            this.LogoutLabel.Location = new System.Drawing.Point(1087, 817);
+            this.LogoutLabel.Location = new System.Drawing.Point(1495, 1226);
+            this.LogoutLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LogoutLabel.Name = "LogoutLabel";
-            this.LogoutLabel.Size = new System.Drawing.Size(60, 20);
+            this.LogoutLabel.Size = new System.Drawing.Size(87, 29);
             this.LogoutLabel.TabIndex = 4;
             this.LogoutLabel.TabStop = true;
             this.LogoutLabel.Text = "Logout";
@@ -121,20 +122,19 @@
             // 
             // SpeakerMainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
-            this.ClientSize = new System.Drawing.Size(1200, 862);
+            this.ClientSize = new System.Drawing.Size(1650, 1293);
             this.Controls.Add(this.LogoutLabel);
             this.Controls.Add(this.TitleLabel);
             this.Controls.Add(this.SubtitleLabel);
             this.Controls.Add(this.NewTalkButton);
             this.Controls.Add(this.CardContainerPanel);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "SpeakerMainForm";
             this.Text = "My applications";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SpeakerMainForm_FormClosed);
-            this.Load += new System.EventHandler(this.SpeakerMainForm_Load);
             this.CardContainerPanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
